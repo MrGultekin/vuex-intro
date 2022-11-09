a72cdd61474b45bad48e35fea577b22d854360ae
MrGultekin <gultekin.de@gmail.com>
2022-11-09 11:34:24 +0100

    Has a problem, event detail comes after second click.

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,3 +1,37 @@
+<!--<template>-->
+<!--  <div v-if="event">-->
+<!--    <h1>{{ event.title }}</h1>-->
+<!--    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>-->
+<!--    <p>{{ event.description }}</p>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  props: ['id'],-->
+<!--  // data() {-->
+<!--  //   return {-->
+<!--  //     event: null-->
+<!--  //   }-->
+<!--  // },-->
+<!--  created() {-->
+<!--    // EventService.getEvent(this.id)-->
+<!--    //   .then(response => {-->
+<!--    //     this.event = response.data-->
+<!--    //   })-->
+<!--    //   .catch(error => {-->
+<!--    //     console.log(error)-->
+<!--    //   }) below code added for the action than computed event function also delete data()-->
+<!--    this.$store.dispatch('fetchEvents', this.id)-->
+<!--  },-->
+<!--  computed: {-->
+<!--    event() {-->
+<!--      return this.$store.state.event-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
 <template>
   <div v-if="event">
     <h1>{{ event.title }}</h1>
@@ -9,20 +43,8 @@
 <script>
 export default {
   props: ['id'],
-  // data() {
-  //   return {
-  //     event: null
-  //   }
-  // },
   created() {
-    // EventService.getEvent(this.id)
-    //   .then(response => {
-    //     this.event = response.data
-    //   })
-    //   .catch(error => {
-    //     console.log(error)
-    //   }) below code added for the action than computed event function also delete data()
-    this.$store.dispatch('fetchEvents', this.id)
+    this.$store.dispatch('fetchEvent', this.id)
   },
   computed: {
     event() {
